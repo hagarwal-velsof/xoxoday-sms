3bb09a4daff004e112220a77801162c0e81a0453
Harsh <hagarwal@velsof.com>
2022-11-07 18:20:08 +0530

    Jobs file updated.

--- src/Jobs/SendSmsRequest.php
+++ src/Jobs/SendSmsRequest.php
@@ -8,6 +8,9 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use  Xoxoday\Sms\Model\XosmsMessage;
+use Illuminate\Support\Facades\Http;
+use Config;
 
 class SendSmsRequest implements ShouldQueue
 {
@@ -33,7 +36,47 @@ class SendSmsRequest implements ShouldQueue
      */
     public function handle()
     {
-        $checkoutDetails = $this->details;
-        die('queue');
+        $smsDetails = $this->details;
+
+         // Fetch values from ENV/Config File
+         $url_sms =  Config('xosms.kaleyra_sms_api_url') . Config('xosms.kaleyra_sms_api_key');
+
+         // Prepare the data to be sent to Gateway
+         $payload = array(
+             'method' => "sms",
+             'to' => $smsDetails['country'] . $smsDetails['mobile'],
+             'sender' => Config('xosms.kaleyra_sms_sender_id'),
+             'message' =>  $smsDetails['template']
+         );
+ 
+         // Send the request to Gateway
+         $response = Http::asForm()->post($url_sms, $payload);
+
+         $sms_sent_flag = 0;
+
+         $result = array();
+ 
+         // Return the response
+         if ($response->status() == 200) {
+             $result = json_decode(json_encode($response->object()), true);
+ 
+             if (isset($result['status']) && $result['status'] == "OK") {
+                $sms_sent_flag = 1;
+             }
+         }
+
+         if($sms_sent_flag == 1){
+            try {
+                $sms_status_update = XosmsMessage::where('id', $smsDetails['sms_id'])->update(['status' => 1, 'response' => json_encode($result)]);
+            } catch (QueryException $ex) {
+                return false;
+            }
+         }else{
+            try {
+                $sms_status_update = XosmsMessage::where('id', $smsDetails['sms_id'])->update(['status' => 2, 'response' => json_encode($result)]);
+            } catch (QueryException $ex) {
+                return false;
+            }
+         }
     }
 }
